bb78867b5bcaf0fb7a9a819769173c52e3208b74
sai0002 <87652882+sai0002@users.noreply.github.com>
2023-10-12 12:56:33 +0530

    Add files via upload

---
+++ Array_And_Shuffel.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Array_And_Shuffel {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        System.out.println("Shuffled array is: " + Arrays.toString(array));
+    }
+    private static void shuffleArray(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+}
